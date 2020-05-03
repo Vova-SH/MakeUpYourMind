75b8f1a835714a30d7928376dcd130f0389e4b09
Vova-SH <Vova-SH@users.noreply.github.com>
2020-05-03 13:32:41 +0700

    Revert finish level

---
+++ Assets/Scripts/LevelSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class LevelSettings : MonoBehaviour
+{
+    public int objectCount = 5;
+    public UnityEvent onCollectAll;
+    private int objectCurrent = 0;
+
+    public void AddObject()
+    {
+        objectCurrent++;
+        if (objectCount <= objectCurrent)
+        {
+            onCollectAll.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ObjectScript : MonoBehaviour
+{
+    private LevelSettings levelSetting;
+    private void Start()
+    {
+        levelSetting = GameObject.FindGameObjectWithTag("GameController").GetComponent<LevelSettings>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<PlayerScript>();
+        if (player != null)
+        {
+            levelSetting.AddObject();
+            Destroy(gameObject);
+        }
+    }
+}
